f26ed07f709c3ea40ba1ea6a31bb9440d802ed2e
xujianjun <303583002@qq.com>
2016-09-08 19:45:54 +0800

    init

---
+++ src/main/java/com/jxlx/jayden/Main.java
@@ -0,0 +1,13 @@
+package com.jxlx.jayden;
+
+/**
+ * @author jianjun.xu
+ * @description
+ * @email jianjun.xu@vipshop.com
+ * @date 16/9/8 下午7:40
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
